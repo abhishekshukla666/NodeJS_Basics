bd9bd6ef79d9ade008c278427431ef8d54da9bb8
Abhishek <in.abhishek.shukla@gmail.com>
2019-02-19 15:36:17 +0530

    First commit

---
+++ nodejs_resetapi/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+// // HTML Form
+app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+const userRoute = require('./routes/user.js')
+
+app.use(userRoute)
+
+app.listen(3003, () => {
+    console.log('Server is up and listing on port 3003')
+})
+
---
+++ nodejs_resetapi/routes/user.js
@@ -0,0 +1,77 @@
+
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'node_api_db'
+})
+
+db.connect((err) => {
+    if (err) {
+        res.send(err)
+    } else {
+        console.log("Connected!")
+    }
+})
+
+const route = express.Router()
+
+route.get('/users', (req, res) => {
+    const queryString = 'select * from users'
+    db.query(queryString, (err, rows, fields) => {
+        if (err) {
+            res.sendStatus(500)
+            return
+        }
+        const user = rows.map((row) => {
+            return {
+                id: row.id,
+                first_name: row.firstName,
+                last_name: row.lastName
+            }
+        })
+        res.json(user)
+    })
+})
+
+
+route.post('/userCreate', (req, res) => {
+    
+    const firstName = req.body.createFirstName
+    const lastName = req.body.createLastName
+    const queryString = 'INSERT INTO users (firstName, lastName) VALUES (?, ?)'
+    db.query(queryString, [firstName, lastName], (err, results, fields) => {
+        if (err) {
+            res.sendStatus(500)
+            return
+        }
+
+        res.send('Inserted into db')
+    })
+
+})
+
+route.get('/users/:id', (req, res) => {
+    console.log('Fetching the user for id: ' + req.params.id)
+    // create connection
+    const userId = req.params.id
+    const queryString = 'select * from users where id=' + userId
+    db.query(queryString, (err, rows, fields) => {
+        if (err) {
+            res.sendStatus(500)
+            return
+        }
+        const user = rows.map((row) => {
+            return {name: row.firstName + ' ' + row.lastName}
+        })
+        res.json(user)
+    })
+})
+
+module.exports = route
